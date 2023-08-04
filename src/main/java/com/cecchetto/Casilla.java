24b2f043371576f1d2017a199eca3560db75415f
RamiroCecchetto <ramirocecchetto123gmail.com>
2023-08-03 23:36:41 -0300

    Se agregaron las fotos de algunas piezas

--- src/main/java/com/cecchetto/Casilla.java
+++ src/main/java/com/cecchetto/Casilla.java
@@ -1,17 +1,26 @@
 package com.cecchetto;
 
+import org.example.Util;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.Color;
 
 import static java.awt.Transparency.TRANSLUCENT;
 
 public class Casilla{
 
     JButton casilla;
+    Color color;
     public Casilla() {
         casilla = new JButton();
-        casilla.setPreferredSize(new Dimension(60, 60));
-        casilla.setBorder(BorderFactory.createLineBorder(Color.WHITE));
+        casilla.setPreferredSize(new Dimension(55, 55));
+        casilla.setBorder(BorderFactory.createLineBorder(java.awt.Color.BLACK));
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+        casilla.setBackground(color);
     }
 
     public JButton getButton() {
--- src/main/java/com/cecchetto/Tablero.java
+++ src/main/java/com/cecchetto/Tablero.java
@@ -2,6 +2,7 @@ package com.cecchetto;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class Tablero extends JPanel{
     int width = 8, height = 8;
@@ -15,16 +16,13 @@ public class Tablero extends JPanel{
                 tablero[x][y] = new Casilla();
 
                 if ((x+y)%2 == 0)
-                    tablero[x][y].getButton().setBackground(Color.WHITE);
+                    tablero[x][y].setColor(Color.WHITE);
                 else
-                    tablero[x][y].getButton().setBackground(Color.BLACK);
+                    tablero[x][y].setColor(Color.BLACK);
 
                 this.add(tablero[x][y].getButton());
             }
         }
-
-
-
     }
 
 }
